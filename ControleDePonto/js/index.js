daad8fe4c4c4f6d0116d82cd5b97cda12e8f2631
izabelle <belle.20000@hotmail.com>
2024-09-10 10:24:13 -0300

    atualizando

--- ControleDePonto/js/index.js
+++ ControleDePonto/js/index.js
@@ -1,23 +1,100 @@
-const diaSemana = document.getElementById("dia-semana");
-const diaMesAno = document.getElementById("dia-mes-ano");
-const horaMinSeg = document.getElementById("hora-min-seg");
-
-diaSemana.textContent = getWeekDay();
-diaMesAno.textContent = getCurrentDate();
-horaMinSeg.textContent = getCurrentHour();
-
-function getWeekDay(){  
+// Declaração de funções
+function getWeekDay() {
     const date = new Date();
-    let days = ("Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado");
+    let days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
     return days[date.getDay()];
 }
 
-function getCurrentHour(){
+function getCurrentDate() {
     const date = new Date();
-    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+
+    const locale = navigator.language;
+
+    let formattedDate;
+    switch (locale) {
+        case 'en-US':
+            formattedDate = `${month}/${day}/${year}`;
+            break;
+        case 'ja-JP':
+            formattedDate = `${year}.${month}.${day}`;
+            break;
+        default:
+            formattedDate = `${day}/${month}/${year}`;
+    }
+
+    return formattedDate;
 }
 
-function getCurrentDate(){
+function getCurrentTime() {
     const date = new Date();
-    return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
 }
+
+function printCurrentHour() {
+    horaMinSeg.textContent = getCurrentTime();
+}
+
+// Referências de elementos DOM
+const diaSemana = document.getElementById("dia-semana");
+const diaMesAno = document.getElementById("dia-mes-ano"); 
+const horaMinSeg = document.getElementById("hora-min-seg");
+
+const btnBaterPonto = document.getElementById("btn-bater-ponto");
+const dialogPonto = document.getElementById("dialog-ponto");
+
+const btnDialogFechar = document.getElementById("btn-dialog-fechar");
+
+const btnDialogBaterPonto = document.getElementById("btn-dialog-bater-ponto");
+
+const dialogData = document.getElementById("dialog-data");
+const dialogHora = document.getElementById("dialog-hora");
+
+
+// Ouvintes de eventos
+btnBaterPonto.addEventListener("click", function() {
+    dialogPonto.showModal();
+});
+
+btnDialogFechar.addEventListener("click", () => {
+    dialogPonto.close();
+});
+
+btnDialogBaterPonto.addEventListener("click", () => {
+    const tipoSelect = document.getElementById("tipos-ponto");
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let info = {
+                data: getCurrentDate(),
+                hora: getCurrentTime(),
+                tipo: tipoSelect.options[tipoSelect.selectedIndex].value,
+                localizacao: {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                }
+            };
+
+            console.log(info); // Para fins de depuração
+        });
+    } else {
+        console.log("Geolocalização não é suportada por este navegador.");
+    }
+});
+
+// Configuração incial
+dialogData.textContent = "Data: " + getCurrentDate();
+dialogHora.textContent = "Hora: " + getCurrentTime();
+
+diaSemana.textContent = getWeekDay();
+diaMesAno.textContent = getCurrentDate();
+horaMinSeg.textContent = getCurrentTime();
+
+setInterval(printCurrentHour, 1000);
+
+
